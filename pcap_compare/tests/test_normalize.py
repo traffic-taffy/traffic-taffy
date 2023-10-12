ade1695fecff438337828020afdf02d1382fc8e7
Wes Hardaker <opensource@hardakers.net>
2023-10-12 14:03:27 -0700

    test for differing gaps

--- pcap_compare/tests/test_normalize.py
+++ pcap_compare/tests/test_normalize.py
@@ -17,3 +17,21 @@ def test_pcap_normalize():
         "a": [1, 0] * 4 + [1],
         "b": [1, 0, 0] * 3,
     }
+
+
+def test_pcap_normalize_with_gaps():
+    pg = PcapGraph(Mock(), Mock(), bin_size=7)
+    data = defaultdict(Counter)
+    for i in range(0, 100, 7):
+        if i % 14 == 0:
+            data["a"][i] += 1
+    for i in range(0, 100, 7):
+        if i % 21 == 0:
+            data["b"][i] += 1
+
+    results = pg.normalize_bins(data)
+    assert results == {
+        "time": list(range(0, 100, 7)),
+        "a": [1, 0] * 7 + [1],
+        "b": [1, 0, 0] * 5,
+    }
