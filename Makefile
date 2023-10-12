e58110bb2dbb9c9863cdfb858d02d86b26d3d326
Wes Hardaker <opensource@hardakers.net>
2023-10-12 13:10:45 -0700

    start at a setup and build process

---
+++ Makefile
@@ -0,0 +1,15 @@
+it:
+	python3 setup.py build
+
+testit:
+	PYTHONPATH=. pytest-3
+
+package: testit
+	rm -rf dist
+	python3 setup.py sdist bdist_wheel
+
+publish:
+	python3 -m twine upload -u __token__ dist/*
+
+install:
+	python3 setup.py install
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcap-compare",
+    version="0.1",
+    author="Wes Hardaker",
+    author_email="opensource@hardakers.net",
+    description="A tool for doing differential analysis of pcap files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hardaker/pcap-compare",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pcap-compare = pcap_compare:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
