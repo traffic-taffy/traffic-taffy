e211afcea74d107d1963bcc9dab88737d046c9c2
Wes Hardaker <opensource@hardakers.net>
2023-10-18 11:22:20 -0700

    import scapy code to the disector

--- pcap_compare/pcap_disector.py
+++ pcap_compare/pcap_disector.py
@@ -1,9 +1,9 @@
 """Loads a PCAP file and counts contents with various levels of storage"""
 
 from enum import Enum
-from logging import warning
+from logging import warning, debug
 from collections import Counter, defaultdict
-
+from scapy.all import rdpcap
 
 class PCAPDisectorType(Enum):
     DETAILED = 1
@@ -68,6 +68,66 @@ class PCAPDisector:
         pcap.dispatch(self.maximum_count, self.dpkt_callback)
         return self.data
 
+    def add_scapy_item(self, field_value, prefix: str) -> None:
+        "Adds an item to the self.data regardless of it's various types"
+        if isinstance(field_value, list):
+            if len(field_value) > 0:
+                # if it's a list of tuples, count the (eg TCP option) names
+                # TODO: values can be always the same or things like timestamps
+                #       that will always change or are too unique
+                if isinstance(field_value[0], tuple):
+                    for item in field_value:
+                        self.data[prefix][item[0]] += 1
+                else:
+                    for item in field_value:
+                        self.add_scapy_item(item, prefix)
+            else:
+                debug(f"ignoring empty-list: {field_value}")
+        elif (
+            isinstance(field_value, str)
+            or isinstance(field_value, int)
+            or isinstance(field_value, float)
+        ):
+            self.data[prefix][field_value] += 1
+
+        elif isinstance(field_value, bytes):
+            try:
+                converted = field_value.decode("utf-8")
+                self.data[prefix][converted] += 1
+            except Exception:
+                converted = "0x" + field_value.hex()
+                self.data[prefix][converted] += 1
+
+    def add_scapy_layer(self, layer, prefix: str | None = "") -> None:
+        "Analyzes a layer to add counts to each layer sub-component"
+
+        if hasattr(layer, "fields_desc"):
+            name_list = [field.name for field in layer.fields_desc]
+        elif hasattr(layer, "fields"):
+            name_list = [field.name for field in layer.fields]
+        else:
+            warning(f"unavailable to deep dive into: {layer}")
+            return
+
+        for field_name in name_list:
+            field_value = getattr(layer, field_name)
+            if hasattr(field_value, "fields"):
+                self.add_scapy_layer(field_value, prefix + field_name + ".")
+            else:
+                self.add_scapy_item(field_value, prefix + field_name)
+
+    def load_via_scapy(self) -> dict:
+        "Loads a pcap file into a nested dictionary of statistical counts"
+        results = defaultdict(Counter)
+        packets = rdpcap(self.pcap_file, count=self.maximum_count)
+
+        for packet in packets:
+            prefix = "."
+            for payload in packet.iterpayloads():
+                prefix = f"{prefix}{payload.name}."
+                self.add_scapy_layer(payload, prefix[1:])
+
+        return results
 
 def main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -85,6 +145,9 @@ def main():
             "-b", "--bin-size", default=1, type=int, help="bin size to use"
         )
 
+        parser.add_argument("-f", "--full-dump", action="store_true",
+                            help="Full deep-inspect the packet")
+
         parser.add_argument(
             "--log-level",
             "--ll",
@@ -100,10 +163,13 @@ def main():
         return args
 
     args = parse_args()
+    disect_type = PCAPDisectorType.COUNT_ONLY
+    if args.full_dump:
+        disect_type = PCAPDisectorType.DETAILED
     pd = PCAPDisector(
         args.input_file,
         bin_size=args.bin_size,
-        disector_type=PCAPDisectorType.COUNT_ONLY,
+        disector_type=disect_type,
         maximum_count=1000,
     )
     pd.load()
