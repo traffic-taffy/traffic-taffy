43d9027e03ca16e6d7d5a98add3172e0ff08c086
Wes Hardaker <opensource@hardakers.net>
2023-11-22 16:02:20 -0800

    actually displays a graph from the file

--- traffic_taffy/explore.py
+++ traffic_taffy/explore.py
@@ -6,7 +6,9 @@ from traffic_taffy.dissector import (
     limitor_add_parseargs,
     check_dissector_level,
 )
+from traffic_taffy.graphdata import PcapGraphData
 from traffic_taffy.compare import PcapCompare
+from PyQt6.QtCharts import QLineSeries, QChart, QChartView
 
 # https://stackoverflow.com/questions/32476006/how-to-make-an-expandable-collapsable-section-widget-in-qt
 
@@ -39,99 +41,59 @@ from traffic_taffy.compare import PcapCompare
 from PyQt6.QtWidgets import (
     QPushButton,
     QDialog,
-    QTreeWidget,
-    QTreeWidgetItem,
+    QGridLayout,
     QVBoxLayout,
     QHBoxLayout,
-    QFrame,
-    QLabel,
     QApplication,
+    QWidget,
 )
 
 
-class SectionExpandButton(QPushButton):
-    """a QPushbutton that can expand or collapse its section"""
-
-    def __init__(self, item, text="", parent=None):
-        super().__init__(text, parent)
-        self.section = item
-        self.clicked.connect(self.on_clicked)
-
-    def on_clicked(self):
-        """toggle expand/collapse of section by clicking"""
-        if self.section.isExpanded():
-            self.section.setExpanded(False)
-        else:
-            self.section.setExpanded(True)
-
-
-class TaffyExplorer(QDialog):
+class TaffyExplorer(QDialog, PcapGraphData):
     """Explore PCAP files by comparison slices"""
 
     def __init__(self, args):
         super().__init__()
-        self.tree = QTreeWidget()
-        self.tree.setHeaderHidden(True)
+
         self.mainLayout = QVBoxLayout()
-        self.mainLayout.addWidget(self.tree)
         self.setLayout(self.mainLayout)
-        self.tree.setIndentation(0)
 
-        self.sections = []
-        self.define_sections()
-        self.add_sections()
+        # create the graph at the top
+        self.detail_graph = QChart()
+        self.detail_graph_view = QChartView(self.detail_graph)
+        self.mainLayout.addWidget(self.detail_graph_view)
 
-        self.plusone = QPushButton("Add one")
-        self.mainLayout.addWidget(self.plusone)
-        self.plusone.clicked.connect(self.addone)
+        # create the mini graph next
+        self.traffic_graph = QChart()
+        self.traffic_graph.legend().hide()
+        self.traffic_graph.setTitle("All Traffic")
+        self.traffic_graph_view = QChartView(self.traffic_graph)
+        self.mainLayout.addWidget(self.traffic_graph_view)
+
+        # create the traffic source menu bar
+        self.source_menus = QHBoxLayout()  # TODO: line graph
+        self.source_menus_w = QWidget()
+        self.source_menus_w.setLayout(self.source_menus)
+        self.mainLayout.addWidget(self.source_menus_w)
+
+        # the comparison panel contains deltas between them
+        self.comparison_panel = QGridLayout()
+        self.comparison_panel_w = QWidget()
+        self.comparison_panel_w.setLayout(self.comparison_panel)
+        self.mainLayout.addWidget(self.comparison_panel_w)
+
+        self.quit_button = QPushButton("Quit")
+        self.mainLayout.addWidget(self.quit_button)
+        self.quit_button.clicked.connect(self.quit)
+
+        # self.tree = QTreeWidget()
+        # self.tree.setHeaderHidden(True)
+        # self.tree.setIndentation(0)
 
         self.args = args
 
-    def addone(self):
-        print("here")
-        self.add_section("new item", QLabel("one thing"))
-
-    def add_section(self, title, widget):
-        button1 = self.add_button(title)
-        section1 = self.add_widget(button1, widget)
-        button1.addChild(section1)
-
-    def add_sections(self):
-        """adds a collapsible sections for every
-        (title, widget) tuple in self.sections
-        """
-        # self.tree.clear()
-        for title, widget in self.sections:
-            self.add_section(title, widget)
-
-    def define_sections(self):
-        """reimplement this to define all your sections
-        and add them as (title, widget) tuples to self.sections
-        """
-        widget = QFrame(self.tree)
-        layout = QHBoxLayout(widget)
-        layout.addWidget(QLabel("Bla"))
-        layout.addWidget(QLabel("Blubb"))
-        title = "Section 1"
-        self.sections.append((title, widget))
-
-    def add_button(self, title):
-        """creates a QTreeWidgetItem containing a button
-        to expand or collapse its section
-        """
-        item = QTreeWidgetItem()
-        self.tree.addTopLevelItem(item)
-        self.tree.setItemWidget(item, 0, SectionExpandButton(item, text=title))
-        return item
-
-    def add_widget(self, button, widget):
-        """creates a QWidgetItem containing the widget,
-        as child of the button-QWidgetItem
-        """
-        section = QTreeWidgetItem(button)
-        section.setDisabled(True)
-        self.tree.setItemWidget(section, 0, widget)
-        return section
+    def quit(self):
+        exit()
 
     def create_comparison(self):
         self.pc = PcapCompare(
@@ -145,10 +107,34 @@ class TaffyExplorer(QDialog):
             cache_results=self.args.cache_pcap_results,
             dissection_level=self.args.dissection_level,
             between_times=self.args.between_times,
+            bin_size=self.args.bin_size,
         )
 
-        # compare the pcaps
-        self.pcap_data = list(self.pc.load_pcaps())
+        # create the graph data storage
+        # and load everything in
+        datasets = list(self.pc.load_pcaps())
+
+        self.data = {}
+        for dataset in datasets:
+            self.data[dataset["file"]] = dataset["data"]
+
+    def update_traffic_chart(self, mult=1):
+        # self.traffic_graph.clear()
+
+        self.match_key = "__TOTAL"
+        self.match_value = None
+        self.minimum_count = 2
+        df = self.merge_datasets()
+
+        series = QLineSeries()
+
+        # TODO: there must be a better way!
+        for index in df.index:
+            series.append(
+                df["time"][index].to_pydatetime().timestamp(), df["count"][index]
+            )
+
+        self.traffic_graph.addSeries(series)
 
     # def show_comparison(self, pcap_one, timestamp_one, pcap_two, timestamp_two):
 
@@ -198,7 +184,7 @@ def parse_args():
         help="Define the logging verbosity level (debug, info, warning, error, ...).",
     )
 
-    parser.add_argument("pcap_files", type=str, nargs="*", help="PCAP files to analyze")
+    parser.add_argument("pcap_files", type=str, nargs="+", help="PCAP files to analyze")
 
     args = parser.parse_args()
     log_level = args.log_level.upper()
@@ -215,6 +201,7 @@ def main():
     app = QApplication(sys.argv)
     window = TaffyExplorer(args)
     window.create_comparison()
+    window.update_traffic_chart()
     window.show()
     sys.exit(app.exec())
 
