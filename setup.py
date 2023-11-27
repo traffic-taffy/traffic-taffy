3662c18bd00b2b521647bc88f52561e9f65ab7ee
Wes Hardaker <opensource@hardakers.net>
2023-11-27 07:51:00 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="traffic-taffy",
-    version="0.3.5",
+    version="0.3.6",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="A tool for doing differential analysis of pcap files",
