58dfe3730a9f6689e6be453b035d102ea9037766
Wes Hardaker <opensource@hardakers.net>
2023-12-04 13:02:58 -0800

    left/right

--- traffic_taffy/tests/test_compare_results.py
+++ traffic_taffy/tests/test_compare_results.py
@@ -3,10 +3,10 @@ from traffic_taffy.compare import PcapCompare
 
 
 def test_compare_results():
-    test_data1 = {0: {"src": Counter({"a": 5, "b": 10})}}  # total = 15
-    test_data2 = {0: {"src": Counter({"a": 15, "c": 15})}}  # total = 30
+    left_data = {0: {"src": Counter({"a": 5, "b": 10})}}  # total = 15
+    right_data = {0: {"src": Counter({"a": 15, "c": 15})}}  # total = 30
 
-    # this should be positive when test_data2 is larger
+    # this should be positive when right_data is larger
     expected = {
         "src": {
             "a": {
@@ -31,6 +31,6 @@ def test_compare_results():
     }
 
     pc = PcapCompare([1, 2])  # bogus files
-    report = pc.compare_dissections(test_data1[0], test_data2[0])
+    report = pc.compare_dissections(left_data[0], right_data[0])
 
     assert report == expected
