7d65dfc47df9e13a470d4ef4530fddc241da46d8
Wes Hardaker <opensource@hardakers.net>
2024-01-17 15:32:27 -0800

    move graph into the tools directory

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     entry_points={
         "console_scripts": [
             "taffy-compare = traffic_taffy.compare:main",
-            "taffy-graph = traffic_taffy.graph:main",
+            "taffy-graph = traffic_taffy.tools.graph:main",
             "taffy-dissect = traffic_taffy.tools.dissect:main",
             "taffy-cache-info = traffic_taffy.tools.cache_info:main",
         ]
