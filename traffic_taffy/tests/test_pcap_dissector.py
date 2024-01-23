902408ff0ef160a6c49f4902a5a4fd6bf9debb1f
Wes Hardaker <opensource@hardakers.net>
2024-01-23 08:35:13 -0800

    start at fixing the dissector test

--- traffic_taffy/tests/test_pcap_dissector.py
+++ traffic_taffy/tests/test_pcap_dissector.py
@@ -1,5 +1,6 @@
 import os
 from traffic_taffy.dissector_engine.dpkt import DissectionEngineDpkt
+from traffic_taffy.dissection import Dissection
 
 
 def test_dissector_load():
@@ -7,56 +8,64 @@ def test_dissector_load():
 
     pd = PCAPDissector("bogus")
     assert isinstance(pd, PCAPDissector)
-    assert pd.data == {0: {}}
+    assert pd.pcap_file == "bogus"
 
 
 def test_dissector_simple_callback():
     from traffic_taffy.dissector import PCAPDissector, PCAPDissectorLevel
 
-    pd = PCAPDissector(
-        "bogus", dissector_level=PCAPDissectorLevel.COUNT_ONLY, bin_size=2
-    )
-
     dpkt_engine = DissectionEngineDpkt("bogus")
     dpkt_engine.init_dissection()
+    dpkt_engine.dissection.bin_size = 2
+    dpkt_engine.dissector_level = PCAPDissectorLevel.COUNT_ONLY
     dpkt_engine.callback(10, b"")
-    assert pd.data == {
-        0: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 1}},
-        10: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 1}},
+
+    TOTAL_COUNT = Dissection.TOTAL_COUNT
+    TOTAL_SUBKEY = Dissection.TOTAL_SUBKEY
+
+    assert dpkt_engine.dissection.data == {
+        0: {TOTAL_COUNT: {TOTAL_SUBKEY: 1}},
+        10: {TOTAL_COUNT: {TOTAL_SUBKEY: 1}},
     }
 
-    pd.dpkt_callback(12, b"")
-    assert dict(pd.data) == {
-        0: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 2}},
-        10: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 1}},
-        12: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 1}},
+    dpkt_engine.callback(12, b"")
+    assert dict(dpkt_engine.dissection.data) == {
+        0: {TOTAL_COUNT: {TOTAL_SUBKEY: 2}},
+        10: {TOTAL_COUNT: {TOTAL_SUBKEY: 1}},
+        12: {TOTAL_COUNT: {TOTAL_SUBKEY: 1}},
     }
 
-    pd.dpkt_callback(11, b"")
-    assert pd.data == {
-        0: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 3}},
-        10: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 2}},
-        12: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 1}},
+    dpkt_engine.callback(11, b"")
+    assert dpkt_engine.dissection.data == {
+        0: {TOTAL_COUNT: {TOTAL_SUBKEY: 3}},
+        10: {TOTAL_COUNT: {TOTAL_SUBKEY: 2}},
+        12: {TOTAL_COUNT: {TOTAL_SUBKEY: 1}},
     }
 
-    save_file = "/tmp/dissector-test.pkl"
+    base_pcap = "/tmp/dissector-test.pcap"
+    save_file = base_pcap + ".taffy"
     if os.path.exists(save_file):
         os.unlink(save_file)
 
-    pd.save(save_file)
+    dpkt_engine.dissection.save(save_file)
 
     # create a new one to make sure it's blank
     pd = PCAPDissector(
-        "bogusx", dissector_level=PCAPDissectorLevel.DETAILED, bin_size=20
+        base_pcap,
+        dissector_level=PCAPDissectorLevel.DETAILED,
+        bin_size=20,
+        cache_results=True,
     )
 
-    pd.load_saved(save_file)
+    pd.load()
 
-    assert pd.data == {
-        0: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 3}},
-        10: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 2}},
-        12: {pd.TOTAL_COUNT: {pd.TOTAL_SUBKEY: 1}},
+    assert pd.dissection.data == {
+        0: {TOTAL_COUNT: {TOTAL_SUBKEY: 3}},
+        10: {TOTAL_COUNT: {TOTAL_SUBKEY: 2}},
+        12: {TOTAL_COUNT: {TOTAL_SUBKEY: 1}},
     }
+
+    os.unlink(save_file)
 
 
 def test_dissector_scapy_callback():
