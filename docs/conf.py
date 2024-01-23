d9e315cd182675eede8cecdf4c5c379012d8c694
Wes Hardaker <opensource@hardakers.net>
2024-01-23 03:08:59 -0800

    version bump 0.4.3

--- docs/conf.py
+++ docs/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "traffic-taffy"
-copyright = "2024, Wes Hardaker"
+copyright = "2024, USC/ISI"
 author = "Wes Hardaker"
-release = "0.4.2"
+release = "0.4.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
