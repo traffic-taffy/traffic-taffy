7ad82fb5817ec1c7f4ce73c231f2f65a22b609f9
Wes Hardaker <opensource@hardakers.net>
2024-01-24 10:45:30 -0800

    new merge test for dissectors

--- traffic_taffy/tests/test_dict_merge.py
+++ traffic_taffy/tests/test_dict_merge.py
@@ -1,5 +1,6 @@
 from traffic_taffy.dissector import pcap_data_merge
 from collections import defaultdict, Counter
+from traffic_taffy.dissection import Dissection
 
 
 def test_pcap_data_merge():
@@ -20,3 +21,57 @@ def test_pcap_data_merge():
             "c": 10,
         }
     }
+
+
+def dissection_merge():
+    d1 = Dissection("bogus")
+    d2 = Dissection("bogus")
+
+    data1 = {
+        0: {
+            "a": {1: 2, 3: 4},
+            "b": {1: 4, 5: 6},
+        },
+        1: {
+            "a": {1: 2, 3: 4},
+        },
+        2: {
+            "a": {1: 2, 3: 4},
+        },
+    }
+    data2 = {
+        0: {
+            "a": {1: 3, 3: 8},
+            "b": {1: 4, 6: 7},
+        },
+        1: {
+            "b": {1: 2, 3: 4},
+        },
+        3: {
+            "a": {1: 2, 3: 4},
+        },
+    }
+    expected = {
+        0: {
+            "a": {1: 5, 3: 12},
+            "b": {1: 8, 5: 6, 6: 7},
+        },
+        1: {
+            "a": {1: 2, 3: 4},
+            "b": {1: 2, 3: 4},
+        },
+        2: {
+            "a": {1: 2, 3: 4},
+        },
+        3: {
+            "a": {1: 2, 3: 4},
+        },
+    }
+
+    d1.data = data1
+    d2.data = data2
+
+    d1.merge(d2)
+
+    result = d1.data
+    assert result == expected
