76b44a14b52dcf5952bd42e9c6b30b702ca85b4c
Wes Hardaker <opensource@hardakers.net>
2024-02-10 07:21:49 -0800

    linting

--- traffic_taffy/output/fsdb.py
+++ traffic_taffy/output/fsdb.py
@@ -1,12 +1,18 @@
+"""A module to output comparison results to an FSDB output."""
 import sys
 import pyfsdb
+from typing import Any
 
 from traffic_taffy.output import Output
 from traffic_taffy.dissection import Dissection
+from traffic_taffy.comparison import Comparison
 
 
 class Fsdb(Output):
-    def __init__(self, *args, **kwargs):
+    """An FSDB report generator."""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Create an FSDB report generator."""
         super().__init__(*args, **kwargs)
         self.console = None
         self.have_done_header = False
@@ -26,14 +32,13 @@ class Fsdb(Output):
         ]
         self.fsdb.converters = [str, str, str, int, int, int, float, float, float]
 
-    def output_start(self, report):
-        "Prints the header about columns being displayed"
+    def output_start(self, report: Comparison) -> None:
+        """Print the header about columns being displayed."""
         # This should match the spacing in print_contents()
         self.in_report = report.title
 
-    def output_record(self, key, subkey, data) -> None:
-        "prints a report to the console"
-
+    def output_record(self, key: str, subkey: Any, data: dict) -> None:
+        """Print a report to the console."""
         subkey = Dissection.make_printable(key, subkey)
         self.fsdb.append(
             [
