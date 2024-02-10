24f7b42e4f74dbf5635c520dbd581fca3cf5811f
Wes Hardaker <opensource@hardakers.net>
2024-02-10 13:40:59 -0800

    convert more code to a namedtuple

--- traffic_taffy/tests/test_compare_results.py
+++ traffic_taffy/tests/test_compare_results.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from traffic_taffy.compare import PcapCompare
+from traffic_taffy.compare import PcapCompare, Report
 
 
 def test_compare_results():
@@ -9,42 +9,42 @@ def test_compare_results():
     # this should be positive when right_data is larger
     expected = {
         "src": {
-            "a": {
-                "total": 20,
-                "left_count": 5,
-                "right_count": 15,
-                "delta_absolute": 15 - 5,
-                "left_percentage": 5.0 / 15.0,
-                "right_percentage": 15.0 / 30.0,
-                "delta_percentage": 15.0 / 30.0 - 5.0 / 15.0,
-            },
-            "b": {
-                "total": -10,  # only in 1
-                "left_count": 10,
-                "right_count": 0,
-                "delta_absolute": 0 - 10,
-                "left_percentage": 10.0 / 15.0,
-                "right_percentage": 0.0,
-                "delta_percentage": -1.0,
-            },
-            "c": {
-                "total": 15,  # only in 2
-                "left_count": 0,
-                "right_count": 15,
-                "delta_absolute": 15 - 0,
-                "left_percentage": 0.0,
-                "right_percentage": 15.0 / 30.0,
-                "delta_percentage": 1.0,
-            },
-            "__NEW_VALUES__": {
-                "total": 2,  # 1 on each side
-                "left_count": 1,  # b
-                "right_count": 1,  # c
-                "delta_absolute": 1 - 1,
-                "left_percentage": 1.0 / 15.0,  # TODO: nuke this
-                "right_percentage": 1.0 / 30.0,  # TODO: nuke this
-                "delta_percentage": 1.0 / 30.0 - 1.0 / 15.0,
-            },
+            "a": Report(
+                total=20,
+                left_count=5,
+                right_count=15,
+                delta_absolute=15 - 5,
+                left_percentage=5.0 / 15.0,
+                right_percentage=15.0 / 30.0,
+                delta_percentage=15.0 / 30.0 - 5.0 / 15.0,
+            ),
+            "b": Report(
+                total=-10,  # only in 1
+                left_count=10,
+                right_count=0,
+                delta_absolute=0 - 10,
+                left_percentage=10.0 / 15.0,
+                right_percentage=0.0,
+                delta_percentage=-1.0,
+            ),
+            "c": Report(
+                total=15,  # only in 2
+                left_count=0,
+                right_count=15,
+                delta_absolute=15 - 0,
+                left_percentage=0.0,
+                right_percentage=15.0 / 30.0,
+                delta_percentage=1.0,
+            ),
+            "__NEW_VALUES__": Report(
+                total=2,  # 1 on each side
+                left_count=1,  # b
+                right_count=1,  # c
+                delta_absolute=1 - 1,
+                left_percentage=1.0 / 15.0,  # TODO: nuke this
+                right_percentage=1.0 / 30.0,  # TODO: nuke this
+                delta_percentage=1.0 / 30.0 - 1.0 / 15.0,
+            ),
         }
     }
 
