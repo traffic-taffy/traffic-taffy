c4400d9ea5835e4c93d78ceb8d2239363e421174
Wes Hardaker <opensource@hardakers.net>
2024-02-29 06:29:16 -0800

    fix printable check for new _ labeling

--- traffic_taffy/tests/test_value_printing.py
+++ traffic_taffy/tests/test_value_printing.py
@@ -3,7 +3,7 @@ from traffic_taffy.dissection import Dissection
 
 def test_printable():
     assert (
-        Dissection.make_printable("Ethernet.IP.dst", b"\x7f\x00\x00\x01") == "127.0.0.1"
+        Dissection.make_printable("Ethernet_IP_dst", b"\x7f\x00\x00\x01") == "127.0.0.1"
     )
 
     assert Dissection.make_printable("badtype", b"\x7f\x00\x00\x01") == "0x7f000001"
