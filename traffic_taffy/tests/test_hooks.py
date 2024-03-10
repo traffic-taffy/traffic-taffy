1712119a85f1b0826ab67eb1ac6ebf4c559ba0cb
Wes Hardaker <opensource@hardakers.net>
2024-03-10 15:14:10 -0700

    test kwargs

--- traffic_taffy/tests/test_hooks.py
+++ traffic_taffy/tests/test_hooks.py
@@ -28,3 +28,19 @@ def test_register_and_call_hook_with_args():
 
     call_hooks("testhook_storage", the_storage, "otherkey", 4)
     assert the_storage == {"testkey": "testvalue", "otherkey": 4}
+
+
+def test_register_and_call_hook_with_kwargs():
+    @register_hook("testhook_storage")
+    def hook_callback(storage={}, key=None, value=None):
+        storage[key] = value
+
+    the_storage = {}
+
+    call_hooks(
+        "testhook_storage", storage=the_storage, key="testkey", value="testvalue"
+    )
+    assert the_storage == {"testkey": "testvalue"}
+
+    call_hooks("testhook_storage", key="otherkey", value=4, storage=the_storage)
+    assert the_storage == {"testkey": "testvalue", "otherkey": 4}
