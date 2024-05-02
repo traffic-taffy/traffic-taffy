019cfd37fced5035b9745f34e69f744c019457d7
Wes Hardaker <opensource@hardakers.net>
2024-05-02 09:07:24 -0700

    minimally working version of kendall correlation

--- traffic_taffy/algorithms/comparecorrelation.py
+++ traffic_taffy/algorithms/comparecorrelation.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from traffic_taffy.algorithms.compareseries import ComparisonSeriesAlgorithm
 from logging import debug
+from rich import print
 
 
 class CompareCorrelation(ComparisonSeriesAlgorithm):
@@ -23,4 +24,14 @@ class CompareCorrelation(ComparisonSeriesAlgorithm):
         debug(f"correlation comparing {column_left} and {column_right}")
         both = pd.concat([series_left, series_right], axis=1)
         results = both.corr(method="kendall")
-        print(results)
+        if results["left"][1] > 0.8:
+            # if results['left'][1] == 1.0:
+            #     import pdb ; pdb.set_trace()
+            print(
+                f"{column_left:<30} similar to {column_right:<30}: {results['left'][1]}"
+            )
+            print(both)
+        else:
+            debug(
+                f"{column_left} not similar to {column_right} with correlation {results['left'][1]}"
+            )
