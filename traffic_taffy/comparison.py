3e6b55738663e7e80728144b5cc60691aa55448e
Wes Hardaker <opensource@hardakers.net>
2024-05-06 08:39:37 -0700

    fix the comparison class for correct typing information

--- traffic_taffy/comparison.py
+++ traffic_taffy/comparison.py
@@ -3,13 +3,21 @@
 from __future__ import annotations
 from typing import Dict, Any
 
+from traffic_taffy.reports import Report
+
+# Organized reports are dicts containing a primary key that is being
+# compared to (left hand side), and a secondary key that is the right
+# hand thing being compared.  Each key/subkey combination should point
+# to a Report containing the results of that comparison.
+OrganizedReports = Dict[str, Dict[Any, Report]]
+
 
 class Comparison:
     """A simple data storage class to hold comparison data."""
 
-    def __init__(self, contents: list, title: str = ""):
+    def __init__(self, contents: OrganizedReports, title: str = ""):
         """Create a Comparison class from contents."""
-        self.contents = contents
+        self.contents: OrganizedReports = contents
         self.title: str = title
         self.printing_arguments: Dict[str, Any] = {}
 
@@ -25,10 +33,10 @@ class Comparison:
 
     # report contents -- actual data
     @property
-    def contents(self) -> None:
+    def contents(self) -> OrganizedReports:
         """The contents of this comparison."""
         return self._contents
 
     @contents.setter
-    def contents(self, new_contents: str) -> None:
+    def contents(self, new_contents: OrganizedReports) -> None:
         self._contents = new_contents
