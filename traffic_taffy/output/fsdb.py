4aa0f66b2dc7347645b2161b73ecd63791e336cd
Wes Hardaker <opensource@hardakers.net>
2024-05-14 16:08:09 -0700

    Fix fsdb output for correlations

--- traffic_taffy/output/fsdb.py
+++ traffic_taffy/output/fsdb.py
@@ -6,6 +6,7 @@ from typing import Any
 from traffic_taffy.output import Output
 from traffic_taffy.dissection import Dissection
 from traffic_taffy.comparison import Comparison
+from traffic_taffy.reports import Report
 
 import dataclasses
 
@@ -34,15 +35,15 @@ class Fsdb(Output):
 
         self.fsdb.out_column_names = [
             "report",
-            "Key",
+            "key",
             "subkey",
         ] + self.columns
         self.fsdb.converters = [str, str, str] + self.converters
 
-    def output_start(self, report: Comparison) -> None:
+    def output_start(self, comparison: Comparison, report: Report) -> None:
         """Print the header about columns being displayed."""
         # This should match the spacing in print_contents()
-        self.in_report = report.title
+        self.in_report = comparison.title
 
     def output_record(self, key: str, subkey: Any, data: dict) -> None:
         """Print a report to the console."""
