8d0bf161646b34fe0353f48d3ab1d228b3aa02c9
Wes Hardaker <opensource@hardakers.net>
2024-05-24 08:59:43 -0700

    lint fixing

--- traffic_taffy/config.py
+++ traffic_taffy/config.py
@@ -1,11 +1,17 @@
-"""A helper class to store a generic set of configuration as a dict"""
+"""A helper class to store a generic set of configuration as a dict."""
 
+from __future__ import annotations
 from enum import Enum
-from typing import TextIO, Dict, Any
-from argparse import Namespace
+from typing import TextIO, Dict, Any, TYPE_CHECKING
+from path import Path
+
+if TYPE_CHECKING:
+    from argparse import Namespace
 
 
 class ConfigStyles(Enum):
+    """A set of configuration types."""
+
     YAML = "yaml"
     TOML = "toml"
     # TODO(hardaker): support "any" at some point to determine type at run-time
@@ -15,11 +21,11 @@ class Config(dict):
     """A generic configuration storage class."""
 
     def __init__(self):
-        pass
+        """Create an configuration object to store collected data in."""
 
     def load_stream(
         self, config_handle: TextIO, style: ConfigStyles = ConfigStyles.YAML
-    ):
+    ) -> None:
         """Import a set of configuration from a IO stream."""
         if style == ConfigStyles.YAML:
             import yaml
@@ -30,13 +36,15 @@ class Config(dict):
 
         self.update(contents)
 
-    def load_file(self, config_file: str, style: ConfigStyles = ConfigStyles.YAML):
-        """Load a configuration file from a filename"""
-        self.load_stream(open(config_file))
+    def load_file(
+        self, config_file: str, style: ConfigStyles = ConfigStyles.YAML
+    ) -> None:
+        """Load a configuration file from a filename."""
+        self.load_stream(Path.open(config_file), style=style)
 
     def load_namespace(
         self, namespace: Namespace, mapping: Dict[str, Any] | None = None
-    ):
+    ) -> None:
         """Load the contents of a namespace into configuration."""
         values = vars(namespace)
         if mapping:
