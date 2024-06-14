b34f4beed727fd63a1ff9da1d19de7547ffbb0d6
Wes Hardaker <opensource@hardakers.net>
2024-06-13 22:58:13 -0700

    new tool to dump config

---
+++ traffic_taffy/tools/config.py
@@ -0,0 +1,65 @@
+"""Performs generic dissection of a PCAP file."""
+import sys
+import logging
+import yaml
+from logging import error
+from traffic_taffy.dissector import (
+    dissector_add_parseargs,
+    limitor_add_parseargs,
+    dissector_handle_arguments,
+    PCAPDissector,
+)
+from traffic_taffy.dissectmany import PCAPDissectMany
+from traffic_taffy.taffy_config import TaffyConfig
+from rich_argparse import RichHelpFormatter
+from argparse import ArgumentParser, Namespace
+
+
+def main() -> None:
+    """Dissect a pcap file and report contents."""
+
+    def parse_args() -> Namespace:
+        """Parse the command line arguments."""
+
+        config: TaffyConfig = TaffyConfig()
+        config.config_option_names = ["-y", "--config"]
+        config["log_level"] = "info"
+
+        config.read_configfile_from_arguments(sys.argv)
+
+        parser = ArgumentParser(
+            formatter_class=RichHelpFormatter,
+            description=__doc__,
+            epilog="Example Usage: taffy-config > defaults.yml",
+        )
+
+        parser.add_argument(
+            "-y",
+            "--config",
+            default=None,
+            type=str,
+            help="Configuration file (YAML) to load.",
+        )
+
+        parser.add_argument(
+            "--log-level",
+            "--ll",
+            default="info",
+            help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).",
+        )
+
+        args = parser.parse_args()
+        log_level = args.log_level.upper()
+        logging.basicConfig(level=log_level, format="%(levelname)-10s:\t%(message)s")
+
+        config.load_namespace(args)
+        return config
+
+    config = parse_args()
+    args = config.as_namespace()
+
+    print(yaml.dump(config))
+
+
+if __name__ == "__main__":
+    main()
