c40faf19e31a165c8cad1bb874c207a1a007b6c6
Wes Hardaker <opensource@hardakers.net>
2024-06-27 14:36:09 -0700

    don't assume any parts at all

--- traffic_taffy/hooks/labels.py
+++ traffic_taffy/hooks/labels.py
@@ -30,6 +30,8 @@ def split_dns_names(dissection: Dissection, **kwargs):
                     parts = value.split(".")
                     if parts[-1] == "":
                         parts = parts[:-1]  # drop the empty end "." split
+                    if len(parts) == 0:
+                        continue
                     dissection.data[timestamp][key + "_tld"][parts[-1]] += count
                     if len(parts) > 1:
                         dissection.data[timestamp][key + "_sld"][parts[-2]] += count
