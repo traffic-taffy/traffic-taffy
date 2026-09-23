21ec0b9aae96d87f59b2ffed4130dd1e16d5a326
Wes Hardaker <opensource@hardakers.net>
2026-09-23 08:15:51 -0700

    version 1.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "traffic-taffy"
 copyright = "2023-2026, USC/ISI"
 author = "Wes Hardaker"
-release = "1.1"
+release = "1.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
